a9c9d5931546492f816be1b716ec575e8c0d2489
阿貴 <hank5678tw@gmail.com>
2026-01-28 17:53:08 +0800

    feat(app): 完成應用程式主入口和全域樣式

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react"
+import ReactDOM from "react-dom/client"
+
+import "./index.css"
+
+import { GlobalErrorBoundary } from "@/components/boundarys/global-error-boundary"
+import { ANTDProvider } from "@/components/providers/antd-provider"
+import { QueryProvider } from "@/components/providers/query-provider"
+import { RouterProvider } from "@/components/providers/router-provider"
+import "@/lib/zod"
+
+const rootElement = document.getElementById("root")!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <GlobalErrorBoundary>
+        <ANTDProvider>
+          <QueryProvider>
+            <RouterProvider />
+          </QueryProvider>
+        </ANTDProvider>
+      </GlobalErrorBoundary>
+    </StrictMode>
+  )
+}
