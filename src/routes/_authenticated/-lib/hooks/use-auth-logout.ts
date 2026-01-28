347ccc41fa3514e488dd1a4f5760a595c1c0c6d4
阿貴 <hank5678tw@gmail.com>
2026-01-29 07:11:40 +0800

    feat(auth): 新增登入 hooks

---
+++ src/routes/_authenticated/-lib/hooks/use-auth-logout.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query"
+
+import { useToast } from "@/hooks/use-toast"
+import { router } from "@/lib/router"
+import { authLogout } from "@/services/auth/logout"
+
+const GOODBYE_MESSAGES = [
+  "期待您的下次光臨，祝您有美好的一天！",
+  "感謝您的使用，再見！",
+  "希望很快能再次見到您！",
+  "祝您一切順利，期待再次為您服務！",
+  "您已成功登出，期待您的下次到來！"
+]
+
+export const useAuthLogout = () => {
+  const { notification } = useToast()
+  return useMutation({
+    mutationFn: authLogout,
+    onSuccess: () => {
+      router.navigate({ to: "/", replace: true })
+      const message = GOODBYE_MESSAGES[Math.floor(Math.random() * GOODBYE_MESSAGES.length)]
+      notification.success({ title: "登出成功", description: message })
+    }
+  })
+}
